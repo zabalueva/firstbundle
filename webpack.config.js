169e5ce8e4688e401eb41785a98c9155da07ac88
zabalueva <age_eva@mail.ru>
2020-11-10 16:11:56 +0300

    Add postcss

--- webpack.config.js
+++ webpack.config.js
@@ -51,10 +51,10 @@ module.exports = {
                 {
                     loader: 'css-loader',
                     options: { sourceMap: true }
-                }, /* {
+                }, {
                     loader: 'postcss-loader',
                     options: { sourceMap: true }
-                }, */ {
+                }, {
                     loader: 'sass-loader',
                     options: { sourceMap: true }
                 }
